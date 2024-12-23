af2f666f77bf5a5e7620afceb4668345e1fb7f92
Glen Marti <martidicianolold@gmail.com>
2024-12-23 12:56:51 +0100

    feat(issues): implement issue submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,50 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import { Button, TextField } from "@radix-ui/themes";
+import React, { useEffect, useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, setValue, handleSubmit } = useForm<IssueForm>();
+
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    register("description");
+  }, [register]);
+
   return (
-    <div className="max-w-xl space-y-3" data-color-mode="light">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <MDEditor textareaProps={{ placeholder: "Description" }} />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+      data-color-mode="light"
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <MDEditor
+        textareaProps={{ placeholder: "Description" }}
+        value={description}
+        onChange={(value) => {
+          setDescription(value || "");
+          setValue("description", value || "");
+        }}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
