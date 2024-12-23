8e7156a5580987041998efa0dc1e3f1d079840aa
Glen Marti <martidicianolold@gmail.com>
2024-12-23 13:10:56 +0100

    feat(issues-form): handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import React, { useEffect, useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
 import { useForm, Controller } from "react-hook-form";
@@ -15,6 +15,7 @@ const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, setValue, handleSubmit } = useForm<IssueForm>();
 
+  const [error, setError] = useState("");
   const [description, setDescription] = useState("");
 
   useEffect(() => {
@@ -22,29 +23,40 @@ const NewIssuePage = () => {
   }, [register]);
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-      data-color-mode="light"
-    >
-      <TextField.Root
-        placeholder="Title"
-        {...register("title")}
-      ></TextField.Root>
-      <MDEditor
-        textareaProps={{ placeholder: "Description" }}
-        value={description}
-        onChange={(value) => {
-          setDescription(value || "");
-          setValue("description", value || "");
-        }}
-      />
+    <div className="max-w-xl space-y-3">
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error ocurred.");
+          }
+        })}
+        data-color-mode="light"
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
+        <MDEditor
+          textareaProps={{ placeholder: "Description" }}
+          value={description}
+          onChange={(value) => {
+            setDescription(value || "");
+            setValue("description", value || "");
+          }}
+        />
 
-      <Button>Submit New Issue</Button>
-    </form>
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
