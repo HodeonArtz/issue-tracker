192bebe1649a412b3d851b49edba003d79dac586
Glen Marti <martidicianolold@gmail.com>
2024-12-23 12:12:55 +0100

    feat(markdown-editor): replace description text-area with markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,12 +2,12 @@
 
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
-
+import MDEditor from "@uiw/react-md-editor";
 const NewIssuePage = () => {
   return (
-    <div className="max-w-xl space-y-3">
+    <div className="max-w-xl space-y-3" data-color-mode="light">
       <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description" />
+      <MDEditor textareaProps={{ placeholder: "Description" }} />
       <Button>Submit New Issue</Button>
     </div>
   );
