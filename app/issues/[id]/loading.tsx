ebbab7b436c4d31795aff5309a652ee7fbeb4fd2
Glen Marti <martidicianolold@gmail.com>
2024-12-24 13:09:43 +0100

    feat(issues-view): add more loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,33 @@
+import { Box, Card, Text, Flex, Heading, Skeleton } from "@radix-ui/themes";
+
 const LoadingIssueDetailPage = () => {
-  return <div>Loading..</div>;
+  return (
+    <Flex direction="column" gap="0.5rem">
+      <Skeleton className="w-fit">
+        <Heading>Lorem ipsum dolor sit amet</Heading>
+      </Skeleton>
+
+      <Flex align="center" gap="1rem" className="mb-4">
+        <Skeleton width="2rem" height="1rem" />
+        <Text>
+          <Skeleton>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </Skeleton>
+        </Text>
+      </Flex>
+
+      <Card className="prose pt-4">
+        <Skeleton>
+          <Box>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            eum, corporis debitis voluptatibus incidunt mollitia aliquid
+            inventore obcaecati veniam? Ipsa reprehenderit cupiditate rem porro
+            magni optio dolor repudiandae eveniet ad?
+          </Box>
+        </Skeleton>
+      </Card>
+    </Flex>
+  );
 };
 
 export default LoadingIssueDetailPage;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,22 @@
+import { Box, Button, Heading, Skeleton } from "@radix-ui/themes";
 import React from "react";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="max-w-xl space-y-3">
+      <Box>
+        <Skeleton>
+          <Heading>Title</Heading>
+        </Skeleton>
+      </Box>
+      <Box>
+        <Skeleton height="13rem" />
+      </Box>
+      <Skeleton className="mt-3">
+        <Button>Submit New Issue</Button>
+      </Skeleton>
+    </div>
+  );
 };
 
 export default LoadingNewIssuePage;
