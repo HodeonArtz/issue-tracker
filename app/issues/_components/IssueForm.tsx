ade8237764fe7db2f111d575b19ebda143de4119
HodeonArtz <martidicianolold@gmail.com>
2024-12-27 08:48:48 +0100

    feat(issues-edit): build edit issue page

---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { ErrorMessage } from "@/app/components";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
+import { Button, Callout, Spinner, TextField } from "@radix-ui/themes";
+import MDEditor from "@uiw/react-md-editor";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+const IssueForm = ({ issue }: { issue?: Issue }) => {
+  const router = useRouter();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+
+  const [error, setError] = useState("");
+  const [description, setDescription] = useState(issue?.description);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      console.log(error);
+      setError(`An unexpected error ocurred`);
+    }
+  });
+
+  useEffect(() => {
+    register("description");
+  }, [register]);
+
+  return (
+    <div className="max-w-xl space-y-3">
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className=" space-y-3" onSubmit={onSubmit} data-color-mode="light">
+        <TextField.Root
+          placeholder="Title*"
+          defaultValue={issue?.title}
+          {...register("title")}
+        ></TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <MDEditor
+          textareaProps={{ placeholder: "Description*" }}
+          value={description}
+          onChange={(value) => {
+            setDescription(value || "");
+            setValue("description", value || "");
+          }}
+        />
+
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default IssueForm;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,76 +1,8 @@
-"use client";
-
-import { ErrorMessage } from "@/app/components";
-import { createIssueSchema } from "@/app/validationSchemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Callout, Spinner, TextField } from "@radix-ui/themes";
-import MDEditor from "@uiw/react-md-editor";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-type IssueForm = z.infer<typeof createIssueSchema>;
+import React from "react";
+import IssueForm from "../_components/IssueForm";
 
 const NewIssuePage = () => {
-  const router = useRouter();
-  const {
-    register,
-    setValue,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema),
-  });
-
-  const [error, setError] = useState("");
-  const [description, setDescription] = useState("");
-
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      await axios.post("/api/issues", data);
-      router.push("/issues");
-    } catch (error) {
-      console.log(error);
-      setError(`An unexpected error ocurred`);
-    }
-  });
-
-  useEffect(() => {
-    register("description");
-  }, [register]);
-
-  return (
-    <div className="max-w-xl space-y-3">
-      {error && (
-        <Callout.Root color="red">
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
-      <form className=" space-y-3" onSubmit={onSubmit} data-color-mode="light">
-        <TextField.Root
-          placeholder="Title*"
-          {...register("title")}
-        ></TextField.Root>
-        <ErrorMessage>{errors.title?.message}</ErrorMessage>
-        <MDEditor
-          textareaProps={{ placeholder: "Description*" }}
-          value={description}
-          onChange={(value) => {
-            setDescription(value || "");
-            setValue("description", value || "");
-          }}
-        />
-
-        <ErrorMessage>{errors.description?.message}</ErrorMessage>
-
-        <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
-        </Button>
-      </form>
-    </div>
-  );
+  return <IssueForm />;
 };
 
 export default NewIssuePage;
