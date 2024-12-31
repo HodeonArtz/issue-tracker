79af37d35f5c2b9d2fb23f588ef50fe798d1f931
HodeonArtz <martidicianolold@gmail.com>
2024-12-31 09:03:04 +0100

    refactor(issues-assign)

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,5 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/api/auth/signin", request.url));
-}
 export const config = {
   matcher: ["/issues/new", "/issues/:id+/edit"],
 };
